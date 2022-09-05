bde4e7385adbc172aca286cfe1b7da528bb20af1
Eduard Gobjila <edikgobjila01@gmail.com>
2022-09-05 16:21:01 +0100

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_132042) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_140341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "baskets", force: :cascade do |t|
+    t.float "total_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_baskets_on_user_id"
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_favourites_on_item_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.text "description"
+    t.string "brand"
+    t.string "volume"
+    t.string "category"
+    t.bigint "shop_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_items_on_shop_id"
+  end
+
+  create_table "order_histories", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "basket_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["basket_id"], name: "index_order_histories_on_basket_id"
+    t.index ["item_id"], name: "index_order_histories_on_item_id"
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +72,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_132042) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "baskets", "users"
+  add_foreign_key "favourites", "items"
+  add_foreign_key "favourites", "users"
+  add_foreign_key "items", "shops"
+  add_foreign_key "order_histories", "baskets"
+  add_foreign_key "order_histories", "items"
 end

58a354ee5612d84b33c73f6a3b15fd816a33ab40
Rory Leslie Melville <rlesliemelville1@gmail.com>
2022-09-05 16:20:00 +0100

    controllers

---
+++ app/controllers/order_historys_controller.rb
@@ -0,0 +1,2 @@
+class OrderHistorysController < ApplicationController
+end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,5 @@
 class Basket < ApplicationRecord
   belongs_to :user
   belongs_to :order_history
+  has_many :items, through: :order_history
 end

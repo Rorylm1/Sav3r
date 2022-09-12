4369121d73d4c33f048ab1d0718a3ebb8d5b2474
Rory Leslie Melville <rlesliemelville1@gmail.com>
2022-09-12 12:27:34 +0100

    put in a quantity change, quite a lot changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_102330) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_094037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_08_102330) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "link"
+    t.integer "quantity"
   end
 
   create_table "order_histories", force: :cascade do |t|

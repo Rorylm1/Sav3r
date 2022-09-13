2a1b49c1318533b4ebad9a877033eeee597c5d9a
Rory Leslie Melville <rlesliemelville1@gmail.com>
2022-09-13 10:48:12 +0100

    CRUD now functioning for basket page

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,5 +1,5 @@
 class Basket < ApplicationRecord
   belongs_to :user
-  has_many :order_histories
+  has_many :order_histories, dependent: :destroy
   has_many :items, through: :order_histories
 end

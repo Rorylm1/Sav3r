c995eb66366bfa7f7d85de1c9c9d1f40f9a7f78c
Rory Leslie Melville <rlesliemelville1@gmail.com>
2022-09-15 11:06:22 +0100

    comign back

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -10,6 +10,9 @@ class OrderHistoriesController < ApplicationController
   def destroy
     @order_history = OrderHistory.find(params[:id])
     @order_history.destroy
+    @basket = Basket.find(params[:id])
+    @items = @basket.order_histories
+    raise
     redirect_to basket_path, notice: 'Item was successfully removed', status: :see_other
 
   end

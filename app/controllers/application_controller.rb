ab9c98870ad9a56b1495e719c7a6e45240e0f22b
Rory Leslie Melville <rlesliemelville1@gmail.com>
2022-09-15 14:34:42 +0100

    major fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,15 @@ class ApplicationController < ActionController::Base
   before_action :check_basket
 
   def check_basket
-
     @basket ||= Basket.find_by(id: session[:basket_id])
 
-    if @basket.nil?
-      @basket = Basket.new
-      @basket.user = current_user
-      @basket.completed = false
-      @basket.save
+      if @basket.nil?
+        @basket = Basket.new
+        @basket.completed = false
+        @basket.save
+        session[:basket_id] = @basket.id
+      end
 
-      session[:basket_id] = @basket.id
-    end
   end
 
   def after_sign_in_path_for(resource)
@@ -25,3 +23,16 @@ class ApplicationController < ActionController::Base
   end
 
 end
+
+# def check_basket
+#   if current_user.present?
+#     if @basket.nil?
+#       @basket = Basket.new
+#       @basket.user = current_user
+#       @basket.completed = false
+#       @basket.save
+#       session[:basket_id] = @basket.id
+#     end
+#     @basket ||= Basket.find_by(id: session[:basket_id])
+#   end
+# end
--- db/migrate/20220905140257_create_baskets.rb
+++ db/migrate/20220905140257_create_baskets.rb
@@ -2,7 +2,7 @@ class CreateBaskets < ActiveRecord::Migration[7.0]
   def change
     create_table :baskets do |t|
       t.float :total_price
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: true, foreign_key: true
 
       t.timestamps
     end
